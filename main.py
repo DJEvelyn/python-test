cb25160c538fd9191c18f3936bedb6ee59a7dc9d
Dylan Evelyn (DJEvelyn) <DylanJamesEvelyn@hotmail.co.uk>
2024-12-17 10:38:15 +0000

    Added test-branch strings

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ valueOne = 3;
 valueTwo = 5;
 total = valueOne + valueTwo;
 print("Value One - {}, Value Two - {}, Total = {}".format(valueOne, valueTwo, total));
+
+# String testing
+names = ["John", "Richard", "Michael"];
+print(names[0]); 
+names[1] = "Rich";
+print(names[1]);
+print(names[0].lower()); 
